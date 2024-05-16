3eb6811bfd4dad5f16500fa593faec6883fe2135
Давид <davidkrivko@gmail.com>
2024-05-17 00:22:20 +0200

    Fix bug

--- main_app/scripts.py
+++ main_app/scripts.py
@@ -74,8 +74,8 @@ async def get_good_bets():
         data=bets
     )
 
-    start_at = pd.to_datetime(datetime.datetime.now() - datetime.timedelta(minutes=1))
-    end_at = pd.to_datetime(datetime.datetime.now() + datetime.timedelta(minutes=1))
+    start_at = (pd.to_datetime(datetime.datetime.now() - datetime.timedelta(minutes=1))) + datetime.timedelta(hours=2)
+    end_at = (pd.to_datetime(datetime.datetime.now() + datetime.timedelta(minutes=1))) + datetime.timedelta(hours=2)
 
     live_f = bets_df["bets4pro_bet_name"].isin(["live_match", "live_game1", "live_game2", "live_game3"])
     live_df = bets_df[live_f]
